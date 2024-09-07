1cb6cf075c6ad5d9af824c2cb5991dc027a0690f
laksamanaap <laksamana.arya1412@gmail.com>
2024-09-07 14:22:45 +0700

    feat : biodata laksamana

---
+++ biodataLaksamana.java
@@ -0,0 +1,21 @@
+package TugasPertemuan2;
+
+/**
+ * biodataLaksamana
+ */
+public class biodataLaksamana {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("-- Biodata Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("----------------------------------------------------------");
+
+        System.out.println("Nama : Laksamana AryaPutra");
+        System.out.println("NIM : Laksamana AryaPutra");
+        System.out.println("Kelas : TI - 1C");
+        System.out.println("No Absen : 14");
+        System.out.println("Prodi : D-IV Teknik Informatika");
+        System.out.println("Alamat : Jln. Bareng Kartini GG 3G No 25 RT.01 Rw.08 Kec.Klojen Kel. Kauman");
+        System.out.println("No HP : 08972836884");
+        System.out.println("Hobi : Traveling, futsal, badminton");
+    }
+}
