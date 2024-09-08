d1b68f59477791956664b598fb95d45732036530
Najla-Nuricia <najlalaudy@gmail.com>
2024-09-08 17:32:48 +0700

    Biodata Najla Nuricia

---
+++ biodataNajla.java
@@ -0,0 +1,15 @@
+public class biodataNajla {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.print("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.print("\n---------------------------------------------------------");
+        System.out.print("\nNama    : Najla Nuricia Laudy");
+        System.out.print("\nNIM     : 244107020091");
+        System.out.print("\nKelas   : TI 1C");
+        System.out.print("\nNo Absen: 20");
+        System.out.print("\nProdi   : D-IV Teknik Informatika");
+        System.out.print("\nAlamat  : Kabupaten Malang");
+        System.out.print("\nNo HP   : 085707078679");
+        System.out.print("\nHobi    : Menggambar");
+    }
+}
