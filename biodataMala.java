a9876f61a47da27d469cd8b1a65d65c139139cf8
Badiatul-06 <badiatulmala@gmail.com>
2024-09-09 10:10:07 +0700

    BiodataMala

---
+++ biodataMala.java
@@ -0,0 +1,19 @@
+/**
+ * biodataMala
+ */
+public class biodataMala {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama        : Badiatul Mala Atis Shufiyati");
+        System.out.println("NIM         : 244107020134");
+        System.out.println("Kelas       : 1C");
+        System.out.println("No Absen    : 05");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Alamat      : Ngadiluwih Kabupaten Kediri");
+        System.out.println("No HP       : 082330886245");
+        System.out.println("Hobi        : Mendengarkan musik");
+    }
+}
